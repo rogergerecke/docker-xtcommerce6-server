ed6eab7a1a17787ebdbbc28dc0059043f1df102a
rogergerecke <roger.gerecke@gmail.com>
2019-03-17 21:36:40 +0100

    Update massage

--- install_xtcommerce.sh
+++ install_xtcommerce.sh
@@ -15,17 +15,18 @@ fi
 # entpacke xtcommerce in den www ordner und lösche die zip datei
 if unzip -o $DATEINAME.zip -d www && rm $DATEINAME.zip
  then
- echo $DATEINAME.zip wurde nach www/$DATEINAME entpackt
+ echo $DATEINAME.zip wurde nach www/$DATEINAME entpackt die Datein werden jetzt nach www verschoben...
  fi
 
 # alle datein aus unterordner in www kopieren
 if mv -f www/$DATEINAME/* www/ && cp -f www/$DATEINAME/.htaccess www/
  then
- echo Alle Datein wurde in www entpackt
+ echo -e "Alle Datein wurden nach www verschoben der Shop ist jetzt ueber http://localhost/ erreichbar. \n\n"
+ echo -e "Wenn Sie hilfe benötigen wechseln Sie auf http://localhost/demo/"
  fi
 
 # lehres verzeichnis löschen
 if rm -r www/$DATEINAME
 then
-echo Altes verzeichnis wurde gelöscht
+echo "Das Temporere Verzeichnis wurde gelöscht!  Führen Sie jetzt noch sudo ./set_folder_premission.sh im Terminal aus"
 fi
