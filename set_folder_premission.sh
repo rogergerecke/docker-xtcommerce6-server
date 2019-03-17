3c475fce0bb8d5cb95bf59ccba4d56f0ea6fdc09
rogergerecke <roger.gerecke@gmail.com>
2019-03-17 21:35:39 +0100

    Set the Folder Premission on Linux

---
+++ set_folder_premission.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo chmod -R 777 www/cache www/templates_c www/export www/xtLogs www/lic www/media www/plugin_cache www/conf/config.php www/xtWizard/logs www/cronjobs
+
+echo "Fertig :-)  Die Verzeichnisberechtigung wurden richtig gesetzt http://localhost/ ist jetzt online"
